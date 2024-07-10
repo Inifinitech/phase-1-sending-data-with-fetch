6ba4c76039984bd9a749258a3fd6f1c7faf85efc
Izaack Kuria <kuriaisac@gmail.com>
2024-07-11 01:34:59 +0300

    fetch()

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({name: userName, email: userEmail})
+    })
+        .then((response) => response.json())
+        .then((object) => console.log(object))
+        .catch((error) => {
+
+        const body = document.querySelector('body');
+//create a div to display the error
+      const errorNotify = document.createElement('div');
+      errorNotify.textContent = `Error: ${error.message}`;
+      
+      body.appendChild(errorNotify);
+});
+}
+submitData('yoh', 'yoh@gmail.com').then(() => console.log('Just checking'));
